b804cd93ac62c85a74f3a2434ea2b9e5c1843bf6
Priya Tiru <priyatiru522@gmail.com>
2020-07-14 19:19:56 +0530

    Find the Percentage

---
+++ percentage.py
@@ -0,0 +1,12 @@
+n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    l = list(student_marks[query_name])
+    no = len(l)
+    s = sum(l)
+    ss = s/no
+    print("%.2f" % ss)
