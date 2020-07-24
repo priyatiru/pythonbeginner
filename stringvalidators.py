2d91f2a6bd92d431bc0582db2e6a2a37e60a5d50
Priya Tiru <priyatiru522@gmail.com>
2020-07-24 17:22:26 +0530

    String Validators

---
+++ stringvalidators.py
@@ -0,0 +1,10 @@
+s=input()
+
+print(any(char.isalnum() for char in s))
+print(any(char.isalpha() for char in s))
+print(any(char.isdigit() for char in s))
+print(any(char.islower() for char in s))
+print(any(char.isupper() for char in s))
+
+
+
