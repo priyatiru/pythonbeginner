90132baf3d98bf3d98e676c064fa006dad4826d9
Priya Tiru <priyatiru522@gmail.com>
2020-08-13 01:30:36 +0530

    Rangoli from Python

---
+++ rangoli.py
@@ -0,0 +1,9 @@
+import string
+def print_rangoli(size):
+    width = 4 * size - 3
+    alpha = string.ascii_lowercase
+    for i in list(range(size))[::-1] + list(range(1, size)):
+        print('-'.join(alpha[size-1:i:-1] + alpha[i:size]).center(width, '-'))
+
+size = int(input())
+print_rangoli(size)
